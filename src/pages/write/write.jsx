8aa6a36ee315508ea3b8db4c6a3dd5f4703d7494
Karan0009 <karansinghk0@gmail.com>
2020-08-18 16:34:39 +0530

    minor tweaks

--- src/pages/write/write.jsx
+++ src/pages/write/write.jsx
@@ -23,7 +23,7 @@ class WritePage extends Component {
         },
         genre: {
           value: "fiction",
-          valid: false,
+          valid: true,
           touched: false,
           validators: [required],
         },
